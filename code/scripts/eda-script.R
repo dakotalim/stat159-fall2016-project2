9530572f2a4f8d0b2538d815f81637d33fc9db94
dakotalim <dakotalim@berkeley.edu>
2016-10-21 16:53:05 -0700

    Dakota added the code folder, only has eda and pre-modelling scripts

---
+++ code/scripts/eda-script.R
@@ -0,0 +1,113 @@
+# load data
+data = read.csv("data/Credit.csv")
+
+# separate into qualitative & quantitative, data[,1] is an index column
+quantitativeCol = data[,c(2:7,12)]
+qualitativeCol = data[,8:11]
+
+# Quantitative analysis
+sink("data/eda-quantitative-output.txt")
+"Minimum, Q1, Median, Mean, Q3, and Max of all quantitative predictors"
+summary(quantitativeCol)
+"IQR of all quantitative predictors"
+apply(quantitativeCol, 2, FUN=IQR)
+"Standard Deviation of all quantitative predictors"
+apply(quantitativeCol, 2, FUN=sd)
+sink()
+
+png("images/income-plots.png")
+hist(quantitativeCol[,1], main = "Histogram of Income", xlab = "Income")
+boxplot(quantitativeCol[,1], horizontal = T, main = "Boxplot of Income")
+dev.off
+
+png("images/limit-plots.png")
+hist(quantitativeCol[,2], main = "Histogram of Limit", xlab = "Limit")
+boxplot(quantitativeCol[,2], horizontal = T, main = "Boxplot of Limit")
+dev.off
+
+png("images/rating-plots.png")
+hist(quantitativeCol[,3], main = "Histogram of Rating", xlab = "Rating")
+boxplot(quantitativeCol[,3], horizontal = T, main = "Boxplot of Rating")
+dev.off
+
+png("images/cards-plots.png")
+hist(quantitativeCol[,4], main = "Histogram of Cards", xlab = "Cards")
+boxplot(quantitativeCol[,4], horizontal = T, main = "Boxplot of Cards")
+dev.off
+
+png("images/age-plots.png")
+hist(quantitativeCol[,5], main = "Histogram of Age", xlab = "Age")
+boxplot(quantitativeCol[,5], horizontal = T, main = "Boxplot of Age")
+dev.off
+
+png("images/education-plots.png")
+hist(quantitativeCol[,6], main = "Histogram of Education", xlab = "Education")
+boxplot(quantitativeCol[,6], horizontal = T, main = "Boxplot of Education")
+dev.off
+
+png("images/balance-plots.png")
+hist(quantitativeCol[,7], main = "Histogram of Balance", xlab = "Balance")
+boxplot(quantitativeCol[,7], horizontal = T, main = "Boxplot of Balance")
+dev.off
+
+# Qualitative Analysis, uses plyr, gridExtra, MASS packages
+
+install.packages('plyr')
+library(plyr)
+install.packages('gridExtra')
+library(gridExtra)
+library(MASS)
+
+genderF = count(qualitativeCol, 'Gender')
+studentF = count(qualitativeCol, 'Student')
+marriedF = count(qualitativeCol, 'Married')
+ethnicityF = count(qualitativeCol, 'Ethnicity')
+genderRF = genderF[,2]/sum(genderF[,2])
+studentRF = studentF[,2]/sum(studentF[,2])
+marriedRF = marriedF[,2]/sum(marriedF[,2])
+ethnicityRF = ethnicityF[,2]/sum(ethnicityF[,2])
+
+
+png("images/gender-freq.png")
+grid.table(cbind(genderF, "rel-freq"= genderRF))
+dev.off()
+
+png("images/student-freq.png")
+grid.table(cbind(studentF, "rel-freq"= studentRF))
+dev.off()
+
+png("images/married-freq.png")
+grid.table(cbind(marriedF, "rel-freq"= marriedRF))
+dev.off()
+
+png("images/ethnicity-freq.png")
+grid.table(cbind(ethnicityF, "rel-freq"= ethnicityRF))
+dev.off()
+
+png("images/gender-counts-barplot.png")
+barplot(table(qualitativeCol[,1]), ylim = c(0,250))
+dev.off()
+
+png("images/student-counts-barplot.png")
+barplot(table(qualitativeCol[,2]), ylim = c(0,400))
+dev.off()
+
+png("images/married-counts-barplot.png")
+barplot(table(qualitativeCol[,3]), ylim = c(0,250))
+dev.off()
+
+png("images/ethnicity-counts-barplot.png")
+barplot(table(qualitativeCol[,4]), ylim = c(0,250))
+dev.off()
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ code/scripts/pre-modeling-script.R
@@ -0,0 +1,32 @@
+# load data
+data = read.csv("data/Credit.csv")
+# Dummy vars
+temp_data = model.matrix(Balance ~ ., data)
+clean_data = cbind(temp_data[ ,-1], Balance = data[,12])
+# mean centering and standardizing
+scaled_data = scale(clean_data, center = TRUE, scale = TRUE)
+
+write.csv(scaled_credit, file = "data/scaled-credit.csv")
+
+####
+# I will now scrable the data
+# the first 300 observations are the training set
+# the next 100 are the test set
+###
+set.seed(100)
+scaled_data = scaled_data[sample(nrow(scaled_data)),]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
