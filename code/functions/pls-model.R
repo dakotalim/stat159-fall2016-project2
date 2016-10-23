147bf30f7a7e9a7e2740e672934e93f6a56fb112
kartikgupta <kartik.gupta@berkeley.edu>
2016-10-23 14:22:43 -0700

    reading data with row.names = 1. Converted y.pred to matrix - earlier it was being saved as array. SquaredError was having problems subtract to different class types. Edited Save() to include file = ..

--- code/functions/pls-model.R
+++ code/functions/pls-model.R
@@ -1,14 +1,15 @@
-# load data, separate into training and test sets 
-# note that the data was randomized in pre-modeling-script.R
-data = read.csv("data/scaled-credit.csv")
-x.train = read.csv("data/xtrain.csv")
-y.train = read.csv("data/ytrain.csv")
-x.test = read.csv("data/xtest.csv")
-y.test = read.csv("data/ytest.csv")
-
 # uses pls package
 install.packages("pls")
 library(pls)
+
+# load data, separate into training and test sets 
+# note that the data was randomized in pre-modeling-script.R
+data = as.matrix(read.csv("data/scaled-credit.csv", row.names = 1))
+x.train = as.matrix(read.csv("data/xtrain.csv", row.names = 1))
+y.train = as.matrix(read.csv("data/ytrain.csv", row.names = 1))
+x.test = as.matrix(read.csv("data/xtest.csv", row.names = 1))
+y.test = as.matrix(read.csv("data/ytest.csv", row.names = 1))
+
 
 # cross validate pls model
 set.seed(1)
@@ -23,15 +24,15 @@ validationplot(plsCV, val.type = "MSEP")
 dev.off()
 
 # use test sets to get test MSE
-y.pred = predict(plsCV, x.test, ncomp = 11)
+y.pred = as.matrix(predict(plsCV, x.test, ncomp = 11))
 squaredError = (y.test-y.pred)^2
 cvPlsMSE = sum(squaredError)/length(squaredError)
 
 # re-fit on FULL DATA
-plsModel = plsr(Balance ~ ., data = data.frame(data[,2:ncol(data)]), scale = T, ncomp = 11)
+plsModel = plsr(Balance ~ ., data = data.frame(data[,1:ncol(data)]), scale = T, ncomp = 11)
 
 # save CV output, best # pls components, and plsModel
-save(c("plsModel", "bestPls", "plsCV"), "data/pls-models.RData")
+save(plsModel, bestPLS, plsCV, file = "data/pls-models.RData")
 
 
 
