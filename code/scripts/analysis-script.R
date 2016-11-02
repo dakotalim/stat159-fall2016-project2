414c158bca6e85f6e40d55b04bee7ee5c1b449ec
kartikgupta <kartik.gupta@berkeley.edu>
2016-11-02 13:02:38 -0700

    Adding analysis scrpit

--- code/scripts/analysis-script.R
+++ code/scripts/analysis-script.R
@@ -1,2 +1,17 @@
 # Data Analysis Script - Using all 5 regressions
+install.packages("broom")
+library(stargazer)
+library(broom)
+library(ggplot2)
 
+load("./data/lm-model.RData")
+load("./data/ridge-models.RData")
+load("./data/lasso-models.RData")
+load("./data/pcr-models.RData")
+load("./data/pls-models.RData")
+
+
+x = tidy(lassoModel)
+y = tidy(ridgeModel)
+tidy(linearModel)
+stargazer(coef(lassoModel))
