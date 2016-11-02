d0ac26eb90bc158b9257b5c0825caf3885f13ea3
Terry Lim <tlim@airbears2-10-142-18-74.airbears2.1918.berkeley.edu>
2016-11-02 11:20:10 -0700

    save mse and linear model

--- code/functions/lm-model.R
+++ code/functions/lm-model.R
@@ -1,10 +1,16 @@
 data = as.matrix(read.csv("data/scaled-credit.csv", row.names = 1))
+x.train = as.matrix(read.csv("data/xtrain.csv", row.names = 1))
+y.train = as.matrix(read.csv("data/ytrain.csv", row.names = 1))
+x.test = as.matrix(read.csv("data/xtest.csv", row.names = 1))
+y.test = as.matrix(read.csv("data/ytest.csv", row.names = 1))
 
 # build linear model, dont use 'X' column
-linearModel = lm(Balance ~ ., data)
+linearModel = lm(y.train ~ x.train)
+linearMSE = sum(linearModel$residuals^2)/length(linearModel$residuals)
+
 
 # save as .RData file
-save("linearModel", "data/lm-model.RData")
+save(linearModel, linearMSE, file = "data/lm-model.RData")
 
 
 
