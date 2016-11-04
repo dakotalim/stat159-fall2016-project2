c1a1a0f898ac0554ee265814c04c4deaf15c3efc
dakotalim <dakotalim54@gmail.com>
2016-11-04 14:33:38 -0700

    finished ridge tests

--- code/tests/test-ridge.R
+++ code/tests/test-ridge.R
@@ -1,1 +1,37 @@
 # ridge tests
+# setwd("Desktop/Dakota/stat159-fall2016-project2")
+library(testthat)
+source("code/functions/ridge-model.R")
+
+context("Check data dimensions")
+test_that("data is 400x12",{
+  expect_equal(ncol(data), 12)
+  expect_equal(nrow(data), 400)
+})
+test_that("x.test is 100x11",{
+  expect_equal(ncol(x.test), 11)
+  expect_equal(nrow(x.test), 100)
+})
+test_that("y.test is 100x1",{
+  expect_equal(ncol(y.test), 1)
+  expect_equal(nrow(y.test), 100)
+})
+test_that("x.train is 300x11",{
+  expect_equal(ncol(x.train), 11)
+  expect_equal(nrow(x.train), 300)
+})
+test_that("y.train is 300x1",{
+  expect_equal(ncol(y.test), 1)
+  expect_equal(nrow(y.test), 300)
+})
+
+context("ensure models are built correctly")
+
+test_that("cvRidgeMSE is a real number",{
+  expect_is(cvRidgeMSE, "numeric")
+})
+test_that("bestRidgeLambda is a real number",{
+  expect_is(bestRidgeLambda, "numeric")
+})
+
+
